624246bc568eaecd05aa555cc5e7f3abd0efaf63
Tóthmihály Martin <b1totmar@vasvari.org>
2025-09-05 09:56:24 +0200

    changes

--- public/script.js
+++ public/script.js
@@ -1,19 +1,14 @@
  const form = document.getElementById('adatForm');
 const clearBtn = document.getElementById('clearData');
+
  function showData() {
       const data = JSON.parse(localStorage.getItem('felhasznaloAdatok'));
       if (data) {
-        document.getElementById('outNev').textContent = data.nev || '-';
-        document.getElementById('outTelepules').textContent = data.telepules || '-';
-        document.getElementById('outIranyitoszam').textContent = data.iranyitoszam || '-';
-        document.getElementById('outKozterulet').textContent = data.kozterulet || '-';
-        document.getElementById('outHazszam').textContent = data.hazszam || '-';
-      } else {
-        document.getElementById('outNev').textContent = '-';
-        document.getElementById('outTelepules').textContent = '-';
-        document.getElementById('outIranyitoszam').textContent = '-';
-        document.getElementById('outKozterulet').textContent = '-';
-        document.getElementById('outHazszam').textContent = '-';
+        document.getElementById('outNev').textContent = data.nev;
+        document.getElementById('outTelepules').textContent = data.telepules;
+        document.getElementById('outIranyitoszam').textContent = data.iranyitoszam;
+        document.getElementById('outKozterulet').textContent = data.kozterulet;
+        document.getElementById('outHazszam').textContent = data.hazszam;
       }
     }
     
