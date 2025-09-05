9774f22221a8ad1071c5289821797b378d6e82a4
Tóthmihály Martin <b1totmar@vasvari.org>
2025-09-05 10:27:29 +0200

    JO A TORLES

--- public/script.js
+++ public/script.js
@@ -1,6 +1,5 @@
  const form = document.getElementById('adatForm');
 const clearBtn = document.getElementById('clearData');
-
  function showData() {
       const data = JSON.parse(localStorage.getItem('felhasznaloAdatok'));
       if (data) {
@@ -9,6 +8,12 @@ const clearBtn = document.getElementById('clearData');
         document.getElementById('outIranyitoszam').textContent = data.iranyitoszam;
         document.getElementById('outKozterulet').textContent = data.kozterulet;
         document.getElementById('outHazszam').textContent = data.hazszam;
+      } else {
+        document.getElementById('outNev').textContent = '-';
+        document.getElementById('outTelepules').textContent = '-';
+        document.getElementById('outIranyitoszam').textContent = '-';
+        document.getElementById('outKozterulet').textContent = '-';
+        document.getElementById('outHazszam').textContent = '-';
       }
     }
     
